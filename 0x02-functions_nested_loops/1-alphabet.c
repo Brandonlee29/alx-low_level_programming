3a9f9e3bc3bdeb017787a62f37166352c9b95fae
Brandonlee29 <leevash203@gmail.com>
2022-06-07 19:43:55 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,38 +4,36 @@
 
 /**
  *
- *  * print_alphabet - check the code.
+ *  * print_alphabet - Entry point
  *
  *   *
  *
- *    * Return: Always 0.
+ *    * Return: 0
  *
  *     */
-
-
 
 void print_alphabet(void)
 
 {
 
-		char alp = 'a';
 
 
 
-			while (alp <= 'z')
+
+		char c = 'a';
+
+
+
+			while (c <= 'z')
 
 					{
 
-								_putchar (alp);
+								_putchar(c);
 
-										alp++;
-
-
+										c++;
 
 											}
 
-				_putchar ('\n');
-
-
+				_putchar('\n');
 
 }
