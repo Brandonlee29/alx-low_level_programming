69b944cd99fb5c51d048c2ba56c203a4e9813248
Brandon lee <leevash203@gmail.com>
2022-10-18 07:46:12 -0700

    Update main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,10 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _MAIN_
+#define _MAIN_
 
-#include <stdio.h>
-#include <stdlib.h>
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-file_io directory.
+ */
 
 int _putchar(char c);
 int _islower(int c);
@@ -25,4 +27,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
